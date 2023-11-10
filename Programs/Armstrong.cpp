758a2c4a4502265e178cc8ba929ab493bfa15ff4
Abhishek Dwivedi <dubeyabhi065@gmail.com>
2023-11-10 16:25:57 +0530

    modify

---
+++ Programs/Armstrong.cpp
@@ -0,0 +1,49 @@
+//--------ARMSTRONG NUMBER--------//
+
+#include<bits/stdc++.h>
+using namespace std;
+
+bool Armstrong(int n)
+{
+    int sum=0;
+    int temp,digit;
+    bool isArmstrong;
+    
+    temp=n;
+
+    while(temp!=0)
+    {
+        digit=temp%10;
+        temp=temp/10;
+        sum=sum + (digit*digit*digit);
+    }
+    if(sum==n)
+    {
+        isArmstrong=true;
+    }
+    else
+    {
+        isArmstrong=false;
+    }
+
+    return isArmstrong;
+
+}
+int main()
+{
+    int n;
+    bool flag;
+    cout<<"Enter the number"<<endl;
+    cin>>n;
+
+    flag=Armstrong(n);
+    if(flag== true)
+    {
+        cout<<"Armstrong number"<<endl;
+    }
+    else
+    {
+        cout<<"Not an Armstrong number"<<endl;
+    }
+
+}
